f264544da7939ca3827e00657f4f7f6b671e587a
Manish Shrestha <manishshtestha472@gmail.com>
2022-12-09 00:22:32 +0545

    Create PrimeOrNot.c

---
+++ Question_9/PrimeOrNot.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, count = 0;
+    printf("Enter a Number to Check if its prime number or not:");
+    scanf("%d", &num);
+    for (int i = 2; i <= (num / 2); ++i)
+    {
+        if (num % i == 0)
+        {
+            count++;
+            break;
+        }
+    }
+    if (count == 0)
+    {
+        printf("%d is a prime number", num);
+    }
+    else
+    {
+        printf("%d is not a prime number", num);
+    }
+} // Main
