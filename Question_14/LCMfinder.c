e7254797fe6d10f849bfe5c25359eb7e30677c85
Manish Shrestha <manishshtestha472@gmail.com>
2022-12-11 19:58:11 +0545

    Create LCMfinder.c

---
+++ Question_14/LCMfinder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, lcm = 1;
+    printf("Enter first number:");
+    scanf("%d", &num1);
+    printf("Enter second number:");
+    scanf("%d", &num2);
+    while (1)
+    {
+        if (lcm % num1 == 0 && lcm % num2 == 0){
+            printf("The LCM of %d and %d is %d",num1,num2,lcm);
+            break;
+        }
+        lcm++;
+    }
+    return 0;
+} // Main
