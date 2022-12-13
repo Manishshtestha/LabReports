e98cda0db95608eb5b5aac2cecf47b6586b24a0e
Manish Shrestha <manishshtestha472@gmail.com>
2022-12-13 20:26:26 +0545

    Create NumbersFrom200to300.c

---
+++ Question_13/NumbersFrom200to300.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int i = 200;
+    printf("Printing numbers from 201 to 300 using while loop");
+    while (i <= 300)
+    {
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}//Main
