acb0b58c5c278ca4b24e3993c1e9801b1e99471f
Manish Shrestha <manishshtestha472@gmail.com>
2022-12-19 21:12:51 +0545

    Update LargestAmong3.c
    Shortened the code u

--- Question_5/LargestAmong3.c
+++ Question_5/LargestAmong3.c
@@ -11,29 +11,18 @@ int main(int argc, char const *argv[])
     scanf("%d", &num3);
     if (num1 > num2)
     {
-        if (num1 > num3)
-        {
-            printf("The number %d is largest among %d,%d,%d", num1, num1, num2, num3);
-        }
-        else{
-            printf("The number %d is largest",num3)
-        }
+        num1 > num3 ? printf("The number %d is largest among %d,%d,%d", num1, num1, num2, num3) : printf("The number %d is largest among %d,%d,%d", num3, num3, num2, num1);
     }
     else if (num2 > num3)
     {
-        if (num3 >= num1)
-        {
-            printf("The number %d is largest among %d,%d,%d", num2, num2, num3, num1);
-        }
+        num2>num1?printf("The number %d is largest among %d,%d,%d", num2, num2, num1, num3):printf("The number %d is largest among %d,%d,%d", num3, num3, num2, num1);
     }
     else if (num3 > num2)
     {
-        if (num2 > num1)
-        {
-            printf("The number %d is largest among %d,%d,%d", num3, num3, num2, num1);
-        }
+        num3>num1?printf("The number %d is largest among %d,%d,%d", num3, num3, num2, num1):printf("The number %d is largest among %d,%d,%d", num1, num1, num2, num3);
     }
-    else{
+    else
+    {
         printf("The numbers are equal");
     }
     return 0;
