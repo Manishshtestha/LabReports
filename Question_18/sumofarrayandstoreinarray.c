55f449c130d58051b42b88e75e52b4813139f817
Manish Shrestha <manishshtestha472@gmail.com>
2022-12-23 00:09:16 +0545

    Update sumofarrayandstoreinarray.c

--- Question_18/sumofarrayandstoreinarray.c
+++ Question_18/sumofarrayandstoreinarray.c
@@ -2,21 +2,14 @@
 
 int main()
 {
-    int a[5], b[5], sum[5];
+    int a[5] = {1,2,3,4,5}, b[5] = {6,7,8,9,4}, sum[5];
     for (int i = 0; i < 5; i++)
     {
-        printf("Enter a number for array (A):", i + 1);
-        scanf("%d", &a[i]);
-    }
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Enter a number for array (B):", i + 1);
-        scanf("%d", &b[i]);
         sum[i] = a[i] + b[i];
     }
     for (int i = 0; i < 5; i++)
     {
-        printf("%d,",sum[i]);
+        printf("%s%d%s",i==0?"[":"",sum[i],i==4?"]":",");
     }
 
 } // Main
