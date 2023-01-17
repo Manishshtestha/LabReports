73835a97dc80359a137d26addfc34b87be3abf2c
Manish Shrestha <manishshtestha472@gmail.com>
2023-01-17 19:20:16 +0545

    Update PalindromeOrNot.c

--- Question_10/PalindromeOrNot.c
+++ Question_10/PalindromeOrNot.c
@@ -2,16 +2,17 @@
 
 int main()
 {
-    int num, revnum = 0, rem, org;
+    int num, reverse = 0, rem, org;
     printf("Enter a number:");
     scanf("%d", &num);
     org = num;
     while (num > 0)
     {
         rem = num % 10;
-        revnum = revnum * 10 + (num % 10);
+        reverse = reverse * 10 + rem;
         num /= 10;
     }
-    revnum == org ? printf("The number %d is Palindrome Number", org) : printf("The number %d is not Palindrome Number", org);
+    reverse == org ? printf("The number %d is Palindrome Number", org) : printf("The number %d is not Palindrome Number", org);
+    return 0;
     /* code here */
 } // Main
