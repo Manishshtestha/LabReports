5316ce15da8f2deb8f54c758794abe7e5ef00e95
Manish Shrestha <manishshtestha472@gmail.com>
2023-01-17 19:20:37 +0545

    Update NumbersFrom200to300.c

--- Question_13/NumbersFrom200to300.c
+++ Question_13/NumbersFrom200to300.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    int i = 200;
-    printf("Printing numbers from 201 to 300 using while loop");
+    int i = 201,j = 201;
+    printf("Printing numbers from 201 to 300 using while loop \n");
     while (i <= 300)
     {
         printf("%d\n",i);
         i++;
     }
+    printf("Printing numbers from 201 to 300 using do while loop \n");
+    do
+    {
+        printf("%d\n",j);
+        j++;
+    } while (j <= 300);
+    
     return 0;
 }//Main
