2d4c6d3fd8c5fe70edee935eea734ed3beaa869e
Manish Shrestha <manishshtestha472@gmail.com>
2023-01-17 19:20:28 +0545

    Update Calculator.c

--- Question_12/Calculator.c
+++ Question_12/Calculator.c
@@ -5,32 +5,24 @@ int main()
     char choice;
     printf("Enter two number:");
     scanf("%d%d",&num1,&num2);
-    printf("What would you like to do?\na.Add\nb.Subtract\nc.Multiply\nd.Divide\ne.exit:");
+    printf("What would you like to do?\na.Add\nb.Subtract\nc.Multiply\nd.Divide:");
     scanf(" %c",&choice);
     switch (choice)
     {
     case 'a':
         printf("The addition of %d and %d is %d",num1,num2,num1+num2);
         break;
-    
     case 'b':
         printf("The difference of %d and %d is %d",num1,num2,num1-num2);
         break;
-    
     case 'c':
         printf("The product of %d and %d is %d",num1,num2,num1*num2);
         break;
-    
     case 'd':
         printf("The division of %d by %d is %d",num1,num2,num1/num2);
         break;
-    
-    case 'e':
-        printf("Terminatting");
-        break;
-    
     default:
-    printf("Invalid Input given");
+        printf("Invalid Input given");
         break;
     }
     return 0;
