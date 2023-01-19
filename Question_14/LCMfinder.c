7620f7490c737ea84eba05e7e82ccf7b7691cd94
Manish Shrestha <manishshtestha472@gmail.com>
2023-01-19 08:08:14 +0545

    Update LCMfinder.c

--- Question_14/LCMfinder.c
+++ Question_14/LCMfinder.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int num1, num2, lcm = 1;
+    int num1, num2, lcm;
     printf("Enter first number:");
     scanf("%d", &num1);
     printf("Enter second number:");
     scanf("%d", &num2);
+    lcm = num1 > num2 ? num1 : num2;
     while (1)
     {
         if (lcm % num1 == 0 && lcm % num2 == 0){
