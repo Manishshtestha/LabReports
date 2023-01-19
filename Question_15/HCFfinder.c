2a9169366382d4b9b442045bd5e3a6de60728fd8
Manish Shrestha <manishshtestha472@gmail.com>
2023-01-19 08:08:10 +0545

    Update HCFfinder.c

--- Question_15/HCFfinder.c
+++ Question_15/HCFfinder.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-
-int main()
-{
+int main(){
     int num1, num2;
-    printf("Enter first number:");
-    scanf("%d", &num1);
-    printf("Enter second number:");
-    scanf("%d", &num2);
-    num1 = (num1 > 0) ? num1 : -num1;
-    num2 = (num2 > 0) ? num2 : -num2;
+    printf("Enter two numbers:");
+    scanf("%d%d", &num1,&num2);
     while (num1 != num2) {
         if(num1 > num2){
             num1 -= num2;
@@ -16,5 +10,6 @@ int main()
             num2 -= num1;
         }
     }
-    printf("The HCF is %d",num1);
+    printf("HCF = %d",num1);
+    return 0;
 } // Main
