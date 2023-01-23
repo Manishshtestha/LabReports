76b308c20c31c91637ec12212278375e1d3fa65e
Manish Shrestha <manishshtestha472@gmail.com>
2023-01-23 07:27:52 +0545

    Update SumOfIntsInArray.c

--- Question_17/SumOfIntsInArray.c
+++ Question_17/SumOfIntsInArray.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 int main(){
     int a[10],sum=0;
+    float avg;
     for (int i = 0; i < 10; i++)
     {
         printf("Enter a number(%d):",i+1);
         scanf("%d",&a[i]);
         sum += a[i];
     }
-    printf("The sum of the given numbers is %d",sum);
+    avg = (float)sum / 10;
+    printf("The sum and average of the given numbers is %d and %0.1f",sum,avg);
     return 0;
 }//Main
