43d216ba5a94377eb1fcc2ef4daaa08bb3c07ad0
Manish Shrestha <manishshtestha472@gmail.com>
2023-01-30 08:00:32 +0545

    Create strlen.c

---
+++ Question_34/strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[50];
+    printf("Enter a random string:");
+    scanf("%s",str);
+    printf("The length of the given string is %d",strlen(str));
+    return 0;
+}//main
+
+
